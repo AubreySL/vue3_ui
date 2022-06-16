39eaef7fd05c6c9e16fcee86326c8e8dfccabfb1
AubreySL <ad81133526@163.com>
2022-06-16 08:41:42 +0800

    feat: add select;

---
+++ src/components/common/Select.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 
+ * @Author: songlin
+ * @Date: 2022-06-15 15:28:54
+ * @LastEditTime: 2022-06-15 15:28:54
+ * @LastEditors: songlin
+-->
+<template>
+  <div>
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+    
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
