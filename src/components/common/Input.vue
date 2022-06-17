7e8f9c999282d72dd907c8c80655a677d645b7d8
AubreySL <AubreySL@163.com>
2022-06-17 09:29:56 +0800

    feat: basic components implement;

---
+++ src/components/common/Input.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Description: 
+ * @Author: songlin
+ * @Date: 2022-06-15 15:28:54
+ * @LastEditTime: 2022-06-16 16:20:14
+ * @LastEditors: songlin
+-->
+<template>
+  <el-input v-model="input" placeholder="Please input" />
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+      input: '123'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/common/Select.vue
+++ src/components/common/Select.vue
@@ -2,18 +2,55 @@
  * @Description: 
  * @Author: songlin
  * @Date: 2022-06-15 15:28:54
- * @LastEditTime: 2022-06-16 10:12:26
+ * @LastEditTime: 2022-06-16 17:06:02
  * @LastEditors: songlin
 -->
 <template>
-  <div>1234</div>
+ <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value"
+    />
+  </el-select>
+  {{value}}
+  <button ref="btnRef" @click="onclick" >{{value}}</button>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {}
-  }
+<script setup>
+import { ref, onMounted } from 'vue'
+const value = ref('')
+
+const options = [
+  {
+    value: 'Option1',
+    label: 'Option1',
+  },
+  {
+    value: 'Option2',
+    label: 'Option2',
+  },
+  {
+    value: 'Option3',
+    label: 'Option3',
+  },
+  {
+    value: 'Option4',
+    label: 'Option4',
+  },
+  {
+    value: 'Option5',
+    label: 'Option5',
+  },
+]
+onMounted(()=>{
+  console.log('select mounted');
+})
+const btnRef = ref(null);
+function onclick(e){
+  console.log(e);
+  console.log(btnRef.value);
 }
 </script>
 
